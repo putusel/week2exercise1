f31a7980cfedcc4167aff14ce9f3ee6d03ee82b2
putusel <89791128+putusel@users.noreply.github.com>
2021-09-03 16:40:45 +0300

    Add files via upload

---
+++ week2exercise1/fi/syksy2021/Week2AllExercises/Week2AllExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.syksy2021.Week2AllExercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2AllExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ week2exercise1/fi/syksy2021/Week2Exercise1/Week2AllExercisesApplication.java
@@ -0,0 +1,13 @@
+package fi.syksy2021.Week2Exercise1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week2AllExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week2AllExercisesApplication.class, args);
+	}
+
+}
